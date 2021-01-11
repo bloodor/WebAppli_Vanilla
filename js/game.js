8a8cd962196bef25804d5efa978af560c7602a9b
Amaury Janneteau <amaury.janneteau@e-artsup.net>
2021-01-11 11:04:50 +0100

    bouncy

--- js/game.js
+++ js/game.js
@@ -1,15 +1,40 @@
 const canvas = document.getElementById('canvasTest');
 const ctx = canvas.getContext('2d');
 
-let x = 10;
+
+let cube = {
+    x: 100,
+    y: 200,
+    xspeed: 3,
+    yspeed: 3,
+    xsize : 50,
+    ysize : 50
+};
+
 function gameLoop()
 {
+    //Draw background
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, 300, 300);
-
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    //Draw red cube
     ctx.fillStyle = 'red';
-    ctx.fillRect(x, 10, 100, 100);
-    x += 0.5
+    ctx.fillRect(cube.x, cube.y, 100, 100);
+    //Update pos
+    cube.x += cube.xspeed
+    cube.y += cube.yspeed
+    hitBorder();
+}
+
+function hitBorder()
+{
+    if (cube.x + 100 >= canvas.width || cube.x <= 0)
+    {
+        cube.xspeed *= -1
+    }
+    if (cube.y + 100 >= canvas.height || cube.y <= 0)
+    {
+        cube.yspeed *= -1
+    }
 }
 
 setInterval(gameLoop, 1000 / 60)
