8c7cfbc551dcdf6ce75f34571dbc7f269994f8e5
Amaury Janneteau <amaury.janneteau@e-artsup.net>
2021-01-11 12:24:32 +0100

    css + immobile circle

--- js/game.js
+++ js/game.js
@@ -1,39 +1,84 @@
 const canvas = document.getElementById('canvasTest');
+
+canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
+canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
+
 const ctx = canvas.getContext('2d');
 
+function arc_create(x, y, r, start, end, dx, dy){
+    let obj = {
+        x: x,
+        y: y,
+        r: r,
+        start: start,
+        end: end,
+        dx: dx,
+        dy: dy
+    }
+    return obj
+}
 
-let cube = {
-    x: 100,
-    y: 200,
-    xspeed: 3,
-    yspeed: 3,
-    xsize : 50,
-    ysize : 50
-};
+function rect_create(x, y, w, h, color, dx, dy) {
+    let obj = {
+        x: x,
+        y: y,
+        w: w,
+        h: h,
+        color: color,
+        dx: dx,
+        dy: dy,
+    }
+    return obj
+}
+
+let rect = rect_create(100, 200, 50, 100, 'red', 5, -2)
+let rect2 = rect_create(300, 50, 100, 50, 'green', -3, 6)
+
+let circle = arc_create(130, 55, 50, 0, 2 * Math.PI)
+let circle2 = arc_create(90, 150, 75, 0, 2 * Math.PI)
 
 function gameLoop()
 {
     //Draw background
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    //Draw red cube
-    ctx.fillStyle = 'red';
-    ctx.fillRect(cube.x, cube.y, 100, 100);
-    //Update pos
-    cube.x += cube.xspeed
-    cube.y += cube.yspeed
-    hitBorder();
+
+    drawRect(rect);
+    drawRect(rect2);
+    hitBorder(rect);
+    hitBorder(rect2);
+    drawCircle(circle);
+    drawCircle(circle2);
+
 }
 
-function hitBorder()
+function drawRect(rect) {
+    //Draw cube
+    ctx.fillStyle = rect.color;
+    ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
+    //Update pos
+    rect.x += rect.dx
+    rect.y += rect.dy
+}
+
+function drawCircle(circle) {
+    //Draw circle
+    ctx.arc(circle.x, circle.y, circle.r, circle.start, circle.end)
+    ctx.fill()
+    //Update pos
+    circle.x += circle.dx
+    circle.y += circle.dy
+}
+
+function hitBorder(rect)
 {
-    if (cube.x + 100 >= canvas.width || cube.x <= 0)
+    if (rect.x + rect.w >= canvas.width || rect.x <= 0)
     {
-        cube.xspeed *= -1
+        rect.dx *= -1
     }
-    if (cube.y + 100 >= canvas.height || cube.y <= 0)
+    if (rect.y + rect.h >= canvas.height || rect.y <= 0)
     {
-        cube.yspeed *= -1
+        rect.dy *= -1
     }
 }
 
