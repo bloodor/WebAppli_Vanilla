c7fd562a977093d61d7a018b933c70d193919fbe
Amaury Janneteau <amaury.janneteau@e-artsup.net>
2021-01-11 10:22:48 +0100

    update

--- js/game.js
+++ js/game.js
@@ -3,3 +3,13 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 1000);
+
+let frame = 0;
+let variable = "Hello";
+
+function gameLoop()
+{
+    console.log("Hello");
+}
+
+setInterval(gameLoop, 1000)
